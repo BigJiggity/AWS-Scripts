76118e8fe7f30e89d09722a9fa928a26083160c9
John Reed <jr@ideasbynature.com>
2023-08-23 01:32:03 -0500

    first commit

---
+++ s3/list_objectby_lastModified/list_objectby_lastmodified.py
@@ -0,0 +1,40 @@
+import boto3
+import datetime
+import pprint
+import io
+from datetime import date, datetime, timedelta
+
+## Set up pretty print for easier reading
+pp = pprint.PrettyPrinter(indent=4)
+
+# Instantiate session with local creds file in ~/.aws/credentials
+session = boto3.Session(profile_name='default')
+
+# Create client object to interact with s3 endpoints
+s3client = session.client('s3')
+
+#get buckets
+bucket_name = s3client.list_buckets()
+
+#iterate through buckets
+for bucket in bucket_name['Buckets']: 
+  # Create reusable Paginator
+  s3_paginator = s3client.get_paginator('list_objects_v2')
+  #Create Page Iterator from paginator
+  page_iterator = s3client.paginate(Bucket='bucket')
+ #Filter results on lastModified date
+  filtered_iterator = page_iterator.search("Contents[?to_string(LastModified)>='\"2023-03-01 00:00:00+00:00\"'].Key")
+
+#Show Contents
+for object in filtered_iterator:
+  pp.pprint(object['Contents'])
+
+
+# # for bucketname in client.buckets.all():
+# #   print(bucket.name)
+# # s3_iterator = s3_paginator.paginate(Bucket=bucketname)
+# # filtered_iterator = s3_iterator.search(
+# #     "Contents[?to_string(LastModified)>='\"2020-08-18 00:00:00+00:00\"'].Key"
+# # )
+# # for key_data in filtered_iterator:
+# #     print(key_data)
