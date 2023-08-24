698ddcfc66c6db6b555408399b57fb68f99e1e6a
John Reed <jr@ideasbynature.com>
2023-08-24 03:41:25 -0500

    modified output to show object with the last time it was modified

--- s3/list_objectby_lastModified/list_objectby_lastmodified.py
+++ s3/list_objectby_lastModified/list_objectby_lastmodified.py
@@ -6,36 +6,31 @@ from datetime import date, datetime, timedelta
 ## Set up pretty print for easier reading
 pp = pprint.PrettyPrinter(indent=4)
 
-# Instantiate session with local creds file in ~/.aws/credentials
+## Use Commented lines for SSO/Session logins for programatic access otherwise script uses default creds in ~/.aws/credentials
+
 #session = boto3.Session(profile_name='default')
 
-# Create client object to interact with s3 endpoints
+## Create client object to interact with s3 endpoints
 #s3client = session.client('s3')
 
+#Create resource
 s3=boto3.resource('s3')
 
-# Print out bucket names
+## Print out bucket names
 for bucket in s3.buckets.all():
     print("Bucket name ", bucket.name)
+    
+    ## Setup Paginate to iterate through buckets/objects
     page=boto3.client('s3')
-# bucket_name = s3client.list_buckets() 
-  #Create reusable Paginator
+    ## Create reusable Paginator
     s3_paginator = page.get_paginator('list_objects_v2')
-    #Create Page Iterator from paginator
-    page_iterator = s3_paginator.paginate(Bucket=bucket.name)
-    #Filter results on lastModified date
-    filtered_iterator = page_iterator.search("Contents[?to_string(LastModified)>='\"2023-03-01 00:00:00+00:00\"'].Key")
-
- #Show Contents
+    ## Create Page Iterator from paginator to list buckets
+    bucket_iterator = s3_paginator.paginate(Bucket=bucket.name)
+    ## Filter results on lastModified date
+    filtered_iterator = bucket_iterator.search("Contents[?to_string(LastModified)>='\"2019-08-01 00:00:00+00:00\"'].Key")
+    
+## Show Contents
 for object in filtered_iterator:
-  pp.pprint(object)
+  pp.pprint(object['LastModified'])
 
-
-# # for bucketname in client.buckets.all():
-# #   print(bucket.name)
-# # s3_iterator = s3_paginator.paginate(Bucket=bucketname)
-# # filtered_iterator = s3_iterator.search(
-# #     "Contents[?to_string(LastModified)>='\"2020-08-18 00:00:00+00:00\"'].Key"
-# # )
-# # for key_data in filtered_iterator:
-# #     print(key_data)
+  
