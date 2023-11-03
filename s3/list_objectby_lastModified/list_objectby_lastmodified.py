c0ffa9a527dccdfd65c865a95d7292f7bc874361
John Reed <jsreed@xmhg6rmvvgx4.tx.lan>
2023-11-03 14:41:33 -0500

    made changes to object counter/csv counter... but something is off

--- s3/list_objectby_lastModified/list_objectby_lastmodified.py
+++ s3/list_objectby_lastModified/list_objectby_lastmodified.py
@@ -27,7 +27,7 @@ if not os.path.exists(data_dir):
     os.makedirs(data_dir)
     
 ## List of buckets to skip during iteration
-skip_buckets = ["analytics-emr-runtime",]
+skip_buckets = ["analytics-emr-runtime","cengage-analytics-platform-prod",]
    
 
 
@@ -63,13 +63,10 @@ def get_bucket_data(buckets: list) -> None:
                     os.makedirs(bucket_dir)
                 logging.info("created: %s... ", bucket_dir)
             
-                 ## Set base counts for objects/csv's
-                object_count = 0
-                csv_count = 1
-                
                 ## Create a CSV file for each bucket
-                csv_file = os.path.join(bucket_dir, f'{bucket.name}_{csv_count}.csv')
-                    
+                csv_file = os.path.join(bucket_dir, f'{bucket.name}.csv')
+                logging.info("Current file is: %s", csv_file)    
+                
                 ## Open CSV in write mode
                 with open (csv_file, 'w', newline='') as file:
                     csv_writer = csv.writer(file)
@@ -87,6 +84,9 @@ def get_bucket_data(buckets: list) -> None:
                         ## Convert last_modified time to year-month-day format
                         lstmod = obj.last_modified.date()                                    
 
+                        ## Set object count to 0
+                        object_count = 0
+                        
                         ## Conditional check for object lastmodified date being 3+ years old
                         if lstmod >= CHECK_DATE:                 
                                 
@@ -94,6 +94,11 @@ def get_bucket_data(buckets: list) -> None:
                             data: list = ['%s' %obj.key, '%s' %obj.last_modified, '%s' %
                                     obj.size, '%s' %obj.storage_class, '%s' %obj.owner]
                             
+                            logging.debug("Writer file is: %s", csv_file)
+                            
+                             ## Set base counts for csv's
+                            csv_count = 0
+
                             ## Write Data to csv
                             csv_writer.writerow(data)
                         
@@ -104,6 +109,9 @@ def get_bucket_data(buckets: list) -> None:
                             if object_count == 10000:
                                 file.close()
                                 
+                                ## Reset the object count and increment the csv count
+                                csv_count += 1
+
                                 ## Create new CSV file with incremented name
                                 csv_file_name = os.path.join(bucket_dir, f'{bucket.name}_{csv_count}.csv')
                                 with open (csv_file_name, 'w', newline='') as new_file:
@@ -113,9 +121,9 @@ def get_bucket_data(buckets: list) -> None:
                                     ## Write Header row
                                     csv_writer.writerow(header)
                                     
-                                ## Reset the object count and increment the csv count
+                                 ## Reset the object count and increment the csv count
                                 object_count = 0
-                                csv_count += 1
+                                
                             
                     ## Close the CSV file for the current bucket
                     file.close()
@@ -128,7 +136,7 @@ if __name__ == "__main__":
                 s3 = boto3.resource('s3')
         except Exception as e:
                 logging.error("Error creating S3 resource. Error: %s \n", e)
-                
+                sys.exit(1)
         buckets = s3.buckets.all()
         
         get_bucket_data(buckets)
