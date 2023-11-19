a6cbfd57f50dc4dbd4b26630c469d6b8dd8d988e
John Reed <jr@ideasbynature.com>
2023-11-19 02:52:02 -0600

    cleaned up comments/formatting for refactor2.py

--- s3/list_objectby_lastModified/refactor2.py
+++ s3/list_objectby_lastModified/refactor2.py
@@ -31,101 +31,95 @@ skip_buckets = ["analytics-emr-runtime", "cengage-analytics-platform-prod", "cl-
                 "cengage-s3-access-logs", "cl-gale-chef-receiver-prod"]
 
 
-
 def process_bucket(bucket):
     """
     Method to process data for a single bucket and store it in a CSV file
     Args:
         bucket (boto3.resources.factory.s3.Bucket): Bucket to process
     """
-   
     # Load the state file
     state = load_state()
-   
+
     bucket_name = bucket.name
-    
+
     # Get the bucket name and last object name from the state
     last_bucket_name = state.get('bucket_name')
     last_object_name = state.get('last_object_name')
-    
-    
-    
+
     # Check for bucket conditions to skip
     folder_path = os.path.join(data_dir, bucket.name)
-    
+
     # Check if bucket is not in the skip_bucket list, process object data in bucket
-    # if bucket.name not in skip_buckets:
-    logging.info("Processing Bucket...: %s", bucket.name)
+    if bucket.name not in skip_buckets:
+        logging.info("Processing Bucket...: %s", bucket.name)
 
-    # Create directories for CSVs
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    logging.info("Created: %s... ", folder_path)
+        # Create directories for CSVs
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        logging.info("Created: %s... ", folder_path)
 
-    # Set defaults for object/csv/data
-    object_count = 0
-    csv_count = 1
-    csv_data = []
-        
-# Iterate/Process through Objects
-    for obj in bucket.objects.filter(Prefix=last_object_name):
+        # Set defaults for object/csv/data
+        object_count = 0
+        csv_count = 1
+        csv_data = []
 
-        # Iterate Objects
-        object_count += 1
+        # Iterate/Process through Objects
+        for obj in bucket.objects.filter(Prefix=last_object_name):
 
-        # Convert last_modified time to year-month-day format
-        lstmod = obj.last_modified.date()
-            
-        # Conditional check for object last modified date being 4+ years old or if object exists in state file
-        if lstmod > CHECK_DATE:
-            
-            # Define variables for data rows
-            csv_data.append([bucket.name, obj.key, obj.size, obj.last_modified, obj.storage_class])
+            # Iterate Objects
+            object_count += 1
 
-            # Check if object count is at or below 1k
-            if object_count % 1000 == 0:
-                
-                # Create a CSV file for each bucket
-                csv_file = os.path.join(folder_path, f'{bucket.name}_{csv_count}.csv')
+            # Convert last_modified time to year-month-day format
+            lstmod = obj.last_modified.date()
 
-                # Open CSV in write mode
-                with open(csv_file, 'w', newline='') as file:
-                    csv_writer = csv.writer(file)
+            # Conditional check for object last modified date being 4+ years old or if object exists in state file
+            if lstmod > CHECK_DATE:
 
-                    # Define values for header row
-                    header = ['Bucket Name', 'File_Name', 'File Size', 'Last_Modified_Date', 'Storage Class']
+                # Define variables for data rows
+                csv_data.append([bucket.name, obj.key, obj.size, obj.last_modified, obj.storage_class])
 
-                    # Write Header to CSV
-                    csv_writer.writerow(header)
+                # Check if object count is at or below 1k
+                if object_count % 1000 == 0:
 
-                    # Write Data to CSV
-                    csv_writer.writerows(csv_data)
+                    # Create a CSV file for each bucket
+                    csv_file = os.path.join(folder_path, f'{bucket.name}_{csv_count}.csv')
 
-                    logging.info('Writing file: %s \n', csv_file)
+                    # Open CSV in write mode
+                    with open(csv_file, 'w', newline='') as file:
+                        csv_writer = csv.writer(file)
 
-                    # Increment CSV count, clear data
-                    csv_count += 1
-                    csv_data = []
+                        # Define values for header row
+                        header = ['Bucket Name', 'File_Name', 'File Size', 'Last_Modified_Date', 'Storage Class']
 
-                    # Update the last object name in the state
-                    state['last_object_name'] = obj.key
-                    state['last_bucket_name'] = bucket.name
-                          
-    # Save the updated state
-    save_state(state)
-    
-    # Define values for header row
-    header = ['Bucket Name', 'File_Name', 'File Size', 'Last_Modified_Date', 'Storage Class']
+                        # Write Header to CSV
+                        csv_writer.writerow(header)
 
-    # Create next CSV file
-    if csv_data:
-        csv_file = os.path.join(folder_path, f'{bucket.name}_{csv_count}.csv')
-        with open(csv_file, 'w', newline='') as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(header)
-            csv_writer.writerows(csv_data)
+                        # Write Data to CSV
+                        csv_writer.writerows(csv_data)
 
-            logging.info('Writing file: %s \n', csv_file)
+                        logging.info('Writing file: %s \n', csv_file)
+
+                        # Increment CSV count, clear data
+                        csv_count += 1
+                        csv_data = []
+
+                        # Update the last object name in the state
+                        state['last_object_name'] = obj.key
+                        state['last_bucket_name'] = bucket.name
+
+        # Save the updated state
+        save_state(state)
+
+        # Create next CSV file
+        if csv_data:
+            csv_file = os.path.join(folder_path, f'{bucket.name}_{csv_count}.csv')
+            with open(csv_file, 'w', newline='') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow(header)
+                csv_writer.writerows(csv_data)
+
+                logging.info('Writing file: %s \n', csv_file)
+
 
 def load_state():
     # Load the state from a file or create a new state if the file doesn't exist
@@ -134,13 +128,15 @@ def load_state():
             state = json.load(file)
     except FileNotFoundError:
         state = {'last_bucket_name': '', 'last_object_name': ''}
-    
+
     return state
+
 
 def save_state(state):
     # Save the state to a file
     with open('state.json', 'w') as file:
         json.dump(state, file)
+
 
 if __name__ == "__main__":
     try:
